8f5498c7ebd064d5363c940337b47ad2d53d8248
LateBloomer34 <himnariyal30@gmail.com>
2025-04-16 10:51:15 +0530

    comment in js

---
+++ 01CommentInJS.js
@@ -0,0 +1,15 @@
+// comment is a note within the code is ignored by the compiler or interprator during execution but is intended for human reader to uderstand the code's purpose or logic.
+//comment gives the explanation about the source code.
+
+
+let a = 100;
+
+
+
+    setTimeout(()=>{
+        for (let i = 0 ; i <= a ; i++)
+        console.log(i)},2000);
+      
+
+
+
